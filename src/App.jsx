b7f19923b917d2b77d7dd75d6433d48e69c94f15
Alexey Svistunov <xdayx53@gmail.com>
2024-03-18 09:51:44 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Button } from "@mui/material";
+import "./App.css";
+import CssBaseline from "@mui/material/CssBaseline";
+
+function App() {
+  return (
+    <>
+      <Button variant="contained">Some text</Button>
+    </>
+  );
+}
+
+export default App;
+
+// сначала накидать скелет
+// потом уже читать документацию maretialUI
+// встраивать ее
+
