8c82a3fc9f098bb5ee401b634a6d31cbb316bc0f
Alexey Svistunov <xdayx53@gmail.com>
2024-03-27 07:37:13 +0300

    add input state, uuid, createMessage function and db

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 export const firebaseConfig = {
   apiKey: "AIzaSyBP6zvxM2NMO-HstQA5gCKS0L2SBm95hOA",
@@ -12,3 +13,4 @@ export const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+export const db = getFirestore(app);
--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,15 +1,35 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useAuth } from "../hooks/use-auth";
+import { useState } from "react";
 
 import { removeUser } from "../store/slices/userSlice";
 
+import { doc, setDoc } from "firebase/firestore"; 
+import { db } from "../firebase/firebase";
+
+import { v4 as uuidv4 } from 'uuid';
+
+
 const ChatPage = () => {
+  const [inputValue, setInputValue] = useState("");
   const dispatch = useDispatch();
   const { isAuth, email } = useAuth();
-  console.log(isAuth);
 
   const logOut = () => {
     dispatch(removeUser());
+  };
+
+  const createMessage = async () => {
+    if(inputValue) {
+      const id = uuidv4()
+      await setDoc(doc(db, 'messages', id), {
+        userEmail: email,
+        message: inputValue,
+        date: new Date()
+      });
+
+      setInputValue('')
+    }
   };
 
   return (
@@ -23,18 +43,43 @@ const ChatPage = () => {
           </div>
 
           <div
-            style={{ border: "1px solid black", width: "80%", height: "80vh", margin: '20px auto', position: 'relative'}}>
-            
-            <div style={{position: 'absolute', bottom: '0', left: '0', width: '100%', height: '100px', borderTop: '1px solid black', display: 'flex', justifyContent: 'flex-end'}}>
-              <input style={{width: '100%', height: '100%', fontSize: '40px', padding: '0 35px'}}></input>
-              <button>Отправить</button>
+            style={{
+              border: "1px solid black",
+              width: "80%",
+              height: "80vh",
+              margin: "20px auto",
+              position: "relative",
+            }}
+          >
+            <div
+              style={{
+                position: "absolute",
+                bottom: "0",
+                left: "0",
+                width: "100%",
+                height: "100px",
+                borderTop: "1px solid black",
+                display: "flex",
+                justifyContent: "flex-end",
+              }}
+            >
+              <input
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  fontSize: "40px",
+                  padding: "0 35px",
+                }}
+              ></input>
+              <button onClick={createMessage}>Отправить</button>
             </div>
           </div>
         </>
       ) : (
         <div>Не Авторизован</div>
       )}
-  
     </>
   );
 };
